f195fbb1140a2eef0c05902da11dcd7aedbb67a1
TaniaGretton <taniagretton@gmail.com>
2023-03-18 16:11:22 +0800

    Yay second challenge is done

--- queries.py
+++ queries.py
@@ -1,16 +1,33 @@
 # pylint:disable=C0111,C0103
 
 def query_orders(db):
-    # return a list of orders displaying each column
-    pass  # YOUR CODE HERE
+    query = """
+    SELECT *
+    FROM Orders
+    ORDER BY OrderID"""
+    db.execute(query)
+    orders = db.fetchall()
+    return orders
 
 def get_orders_range(db, date_from, date_to):
+    query = """
+    SELECT *
+    FROM Orders
+    WHERE OrderDate > ? AND OrderDate <= ?
+    """
+    db.execute(query, (f'{date_from}', f'{date_to}'))
+    orders_range = db.fetchall()
+    return orders_range
     # return a list of orders displaying all columns with OrderDate between
     # date_from and date_to (excluding date_from and including date_to)
-    pass  # YOUR CODE HERE
+
 
 def get_waiting_time(db):
-    # get a list with all the orders displaying each column
-    # and calculate an extra TimeDelta column displaying the number of days
-    # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    pass  # YOUR CODE HERE
+    query = """
+    SELECT *, (julianday(ShippedDate) - julianday(OrderDate)) AS TimeDelta
+    FROM Orders
+    ORDER BY TimeDelta
+    """
+    db.execute(query)
+    orders_time = db.fetchall()
+    return orders_time
